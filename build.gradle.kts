639f99949d2cfb5402f527fc647587791cca9467
Dareiko <darei.dixius@gmail.com>
2026-04-08 08:20:36 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
-// Tento súbor nechaj prázdny alebo tam vlož len toto:
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
+}
+
+// Tento blok musí zostať prázdny, aby sa nebil so settings.gradle.kts
+allprojects {
 }
